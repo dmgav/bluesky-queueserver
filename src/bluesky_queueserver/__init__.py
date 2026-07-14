68cc382cb5baea0b0be5c7eb480649ea260d95a8
Dmitri Gavrilov <gavrilov.dvs@gmail.com>
2026-07-14 09:47:46 -0400

    CI: tests running with pixi locally

--- src/bluesky_queueserver/__init__.py
+++ src/bluesky_queueserver/__init__.py
@@ -1,7 +1,4 @@
-from ._version import get_versions
-
-__version__ = get_versions()["version"]
-del get_versions
+from ._version import __version__
 
 from .manager.annotation_decorator import parameter_annotation_decorator  # noqa: E402, F401
 from .manager.comms import (  # noqa: E402, F401
