c56a971647edbca59d6aa1897d535c444dc1f4c9
Dmitri Gavrilov <gavrilov.dvs@gmail.com>
2026-08-07 11:37:38 -0400

    CI: print the number of open file descriptors after the tests are completed

--- src/bluesky_queueserver/manager/tests/conftest.py
+++ src/bluesky_queueserver/manager/tests/conftest.py
@@ -1,6 +1,28 @@
+import os
+import time as ttime
+
 import pytest
 
 from bluesky_queueserver.manager.profile_ops import clear_registered_items
+
+
+@pytest.fixture(scope="session", autouse=True)
+def print_open_file_descriptors():
+    yield
+    ttime.sleep(1)
+    pid = os.getpid()
+    fd_dir = f"/proc/{pid}/fd"
+    fd_entries = sorted(os.listdir(fd_dir), key=int)
+    msg = f"\n+++ PID={pid} OPEN FILE DESCRIPTORS = {len(fd_entries)}\n"
+    # /dev/tty bypasses all pytest capture layers
+    try:
+        with open("/dev/tty", "w") as tty:
+            tty.write(msg)
+    except OSError:
+        import sys
+
+        sys.stderr.write(msg)
+        sys.stderr.flush()
 
 
 @pytest.fixture(autouse=True)
