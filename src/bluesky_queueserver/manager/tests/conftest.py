c7928e388c937554f32afd90943c902d3314bb52
Dmitri Gavrilov <gavrilov.dvs@gmail.com>
2026-08-07 13:40:31 -0400

    TST: modify 'print_open_file_descriptors' to work on CI

--- src/bluesky_queueserver/manager/tests/conftest.py
+++ src/bluesky_queueserver/manager/tests/conftest.py
@@ -39,19 +39,21 @@ def close_dangling_event_loops():
 
 
 @pytest.fixture(scope="session", autouse=True)
-def print_open_file_descriptors():
+def print_open_file_descriptors(request):
     yield
     ttime.sleep(1)
     pid = os.getpid()
     fd_dir = f"/proc/{pid}/fd"
     fd_entries = sorted(os.listdir(fd_dir), key=int)
-    msg = f"\n+++ PID={pid} OPEN FILE DESCRIPTORS = {len(fd_entries)}\n"
-    # /dev/tty bypasses all pytest capture layers
+    msg = f"+++ PID={pid} OPEN FILE DESCRIPTORS = {len(fd_entries)}"
+    # terminalreporter works on CI (it is supposed to work locally, but it doesn't)
+    reporter = request.config.pluginmanager.get_plugin("terminalreporter")
+    reporter.write_line(msg)
+    # /dev/tty bypasses all pytest capture layers (local use only)
     try:
         with open("/dev/tty", "w") as tty:
-            tty.write(msg)
+            tty.write("\n" + msg + "\n")
     except OSError:
         import sys
-
-        sys.stderr.write(msg)
+        sys.stderr.write("\n" + msg + "\n")
         sys.stderr.flush()
