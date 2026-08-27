099c56b3c846b1011e15b8d57b82935e2549840e
Dmitri Gavrilov <gavrilov.dvs@gmail.com>
2026-08-27 16:42:26 -0400

    TST: additional unit test for device_progress streaming

--- src/bluesky_queueserver/manager/tests/test_info_streaming.py
+++ src/bluesky_queueserver/manager/tests/test_info_streaming.py
@@ -1,3 +1,4 @@
+import pprint
 import threading
 import time as ttime
 
@@ -6,8 +7,13 @@ import pytest
 from bluesky_queueserver.manager.output_streaming import ReceiveSystemInfo, _default_zmq_info_topic
 
 from .common import (
+    _user,
+    _user_group,
+    append_code_to_last_startup_file,
     condition_environment_closed,
     condition_environment_created,
+    condition_queue_processing_finished,
+    copy_default_profile_collection,
     re_manager_cmd,  # noqa: F401
     use_zmq_encoding_for_tests,
     wait_for_condition,
@@ -45,6 +51,12 @@ class ReceiveMessages(threading.Thread):
 
     def unsubscribe(self):
         self._rco.unsubscribe()
+
+    def filter_msgs(self, key):
+        """
+        Returns a list of messages of the specific type (with matching key).
+        """
+        return [_ for _ in self.received_msgs if key in _["msg"].keys()]
 
 
 # fmt: off
@@ -111,6 +123,85 @@ def test_zmq_info_streaming_1(monkeypatch, re_manager_cmd, stream_enabled):  # n
 
     if stream_enabled is not True:
         assert len(rm_info.received_msgs) == 0
+
+    rm_info.stop()
+    rm_info.join()
+
+
+_script_device_progress = """
+
+from ophyd_async import sim
+from ophyd_async.core import init_devices
+
+with init_devices():
+    sim_motor = sim.SimMotor(name="sim_motor", instant=False)
+"""
+
+
+# fmt: off
+@pytest.mark.parametrize("stream_dev_progress_enabled", [True, False, None])
+# fmt: on
+def test_zmq_info_streaming_2(tmp_path, monkeypatch, re_manager_cmd, stream_dev_progress_enabled):  # noqa: F811
+    """
+    Test 0MQ streaming functionality: streaming of device progress.
+    Test that streaming of device progress can be disabled.
+    """
+
+    pc_path = copy_default_profile_collection(tmp_path)
+
+    # Add extra plan. The original set of startup files will not contain this plan.
+    append_code_to_last_startup_file(pc_path, additional_code=_script_device_progress)
+
+    address_info_server = "tcp://*:60621"
+    address_info_client = "tcp://localhost:60621"
+
+    params_server = [f"--zmq-info-addr={address_info_server}", "--zmq-publish-info=ON", f"--startup-dir={pc_path}"]
+    if stream_dev_progress_enabled is not None:
+        params_server.append(f"--zmq-stream-device-progress={'ON' if stream_dev_progress_enabled else 'OFF'}")
+
+    zmq_encoding = use_zmq_encoding_for_tests()
+
+    rm_info = ReceiveMessages(
+        receiver_class=ReceiveSystemInfo,
+        zmq_subscribe_addr=address_info_client,
+        zmq_topic=_default_zmq_info_topic,
+        encoding=zmq_encoding,
+    )
+    msg_key = "device_progress"
+
+    rm_info.start()
+
+    re_manager_cmd(params_server)
+
+    msgs = rm_info.filter_msgs(msg_key)
+    assert len(msgs) == 0
+
+    zmq_request("environment_open")
+    assert wait_for_condition(time=timeout_env_open, condition=condition_environment_created)
+
+    _plan_mv = {"name": "mv", "args": ["sim_motor", 5.0], "item_type": "plan"}
+    params = {"item": _plan_mv, "user": _user, "user_group": _user_group}
+    resp2, _ = zmq_request("queue_item_add", params)
+    assert resp2["success"] is True, f"resp={resp2}"
+
+    resp3, _ = zmq_request("queue_start")
+    assert resp3["success"] is True
+
+    assert wait_for_condition(time=60, condition=condition_queue_processing_finished)
+
+    zmq_request("environment_close")
+    assert wait_for_condition(time=3, condition=condition_environment_closed)
+
+    msgs = rm_info.filter_msgs(msg_key)
+    print(f"msgs = {pprint.pformat(msgs)}")
+    if stream_dev_progress_enabled is True:
+        assert len(msgs) > 3
+        assert msgs[0]["msg"]["device_progress"]["name"] == "sim_motor"
+        assert msgs[0]["msg"]["device_progress"]["current"] == 0.0
+        assert msgs[-2]["msg"]["device_progress"]["current"] == 5.0
+        assert msgs[-1]["msg"]["device_progress"]["completed"] is True
+    else:
+        assert len(msgs) == 0
 
     rm_info.stop()
     rm_info.join()
